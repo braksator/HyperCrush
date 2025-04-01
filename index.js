243415d34d9a07e114fd88f101926294e631a251
braksator <braksator@users.noreply.github.com>
2025-04-01 12:31:17 +1000

    Added frontend gui

--- index.js
+++ index.js
@@ -7,6 +7,31 @@
  */
 
 /**
+ * Processes HTML code.
+ * @param {string} code - (optional) The raw input code.
+ * @returns {string} - The crushed output code.
+ */
+function hypercrushCode(code, mode = 'all') {
+  if (mode == 'whitespace' || mode == 'all') {
+    code = code
+      .replace(/\s+/g, ' ').trim(); // Remove redundant whitespace
+  }
+  if (mode == 'default' || mode == 'all') {
+    code = code
+      .replace(/(<\w+[^>]*\b\w+=['"]?)0\.(\d)/g, '$1.$2') // Remove leading zero for decimals in attribute values inside tags
+      .replace(/>\s+</g, '><') // Remove spaces between tags - Gotcha: Can't rely on whitespace between tags for styling
+      .replace(/(?<=<[^>]+)\s+(?=>)/g, '') // Remove space before > in tags
+      .replace(/(<[a-zA-Z][^>]*>)\s+/g, '$1') // Remove whitespace after opening tags
+      .replace(/\s+(<\/[a-zA-Z]+>)/g, '$1') // Remove whitespace before closing tags
+      .replace(/(\w+)="([^"\s]+)(?="(?!\/>))"/g, (m, k, v) => `${k}=${v}`) // Remove " around attrs where possible (but not if followed by self-close)
+      .replace(/"\s+(?=\s*[\w-]+=|\s*\/?>)/g, '"') // Remove spaces **after** a closing quote (but not if followed by self-close)
+      .replace(/(?<=\w=")\s+/g, '') // Remove spaces **after** an opening quote
+      .replace(/\s*(["']?)\s*\/>/g, '$1/>'); // Remove extra space at end of self-closing tags
+  }
+  return code;
+}
+
+/**
  * Gulp-compatible transform stream.
  * @param {string} mode - (optional) The mode to operate in.
  * @returns {Transform} - A transform stream for Gulp.
@@ -20,24 +45,7 @@ module.exports = (mode = 'default') => {
       if (file.isNull()) return cb(null, file);
       if (file.isStream()) return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
       try {
-        let code = file.contents.toString();
-        if (mode == 'whitespace' || mode == 'all') {
-          code = code
-            .replace(/\s+/g, ' ').trim(); // Remove redundant whitespace
-        }
-        if (mode == 'default' || mode == 'all') {
-          code = code
-            .replace(/(<\w+[^>]*\b\w+=['"]?)0\.(\d)/g, '$1.$2') // Remove leading zero for decimals in attribute values inside tags
-            .replace(/>\s+</g, '><') // Remove spaces between tags - Gotcha: Can't rely on whitespace between tags for styling
-            .replace(/(?<=<[^>]+)\s+(?=>)/g, '') // Remove space before > in tags
-            .replace(/(<[a-zA-Z][^>]*>)\s+/g, '$1') // Remove whitespace after opening tags
-            .replace(/\s+(<\/[a-zA-Z]+>)/g, '$1') // Remove whitespace before closing tags
-            .replace(/(\w+)="([^"\s]+)(?="(?!\/>))"/g, (m, k, v) => `${k}=${v}`) // Remove " around attrs where possible (but not if followed by self-close)
-            .replace(/"\s+(?=\s*[\w-]+=|\s*\/?>)/g, '"') // Remove spaces **after** a closing quote (but not if followed by self-close)
-            .replace(/(?<=\w=")\s+/g, '') // Remove spaces **after** an opening quote
-            .replace(/\s*(["']?)\s*\/>/g, '$1/>'); // Remove extra space at end of self-closing tags
-        }
-        file.contents = Buffer.from(code);
+        file.contents = Buffer.from(hypercrushCode(file.contents.toString()));
         cb(null, file);
       }
       catch (err) {
